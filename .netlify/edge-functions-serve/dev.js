48d1005dd5f3a67014ac70f342e2f076a101d963
kunlexyz <bobbykay007@gmail.com>
2026-04-21 17:45:08 +0100

    change url in fetch API
    TO /.netlify/functions/create-payment

--- .netlify/edge-functions-serve/dev.js
+++ .netlify/edge-functions-serve/dev.js
@@ -4,16 +4,48 @@ const functions = {}; const metadata = { functions: {} };
 
 
       try {
-        const { default: func } = await import("file:///C:/Users/kunle/Documents/web/kunle/public/video%20tutorials/netlify/edge-functions/hello.js");
+        const { default: func } = await import("file:///C:/Users/kunle/Documents/GitHub/semicas/netlify/edge-functions/create-payment.js");
+
+        if (typeof func === "function") {
+          functions["create-payment"] = func;
+          metadata.functions["create-payment"] = {"url":"file:///C:/Users/kunle/Documents/GitHub/semicas/netlify/edge-functions/create-payment.js"}
+        } else {
+          console.log("\u001b[91m⬥\u001b[39m \u001b[31mFailed\u001b[39m to load Edge Function \u001b[33mcreate-payment\u001b[39m. The file does not seem to have a function as the default export.");
+        }
+      } catch (error) {
+        console.log("\u001b[91m⬥\u001b[39m \u001b[31mFailed\u001b[39m to run Edge Function \u001b[33mcreate-payment\u001b[39m:");
+        console.error(error);
+      }
+      
+
+
+      try {
+        const { default: func } = await import("file:///C:/Users/kunle/Documents/GitHub/semicas/netlify/edge-functions/hello.js");
 
         if (typeof func === "function") {
           functions["hello"] = func;
-          metadata.functions["hello"] = {"url":"file:///C:/Users/kunle/Documents/web/kunle/public/video%20tutorials/netlify/edge-functions/hello.js"}
+          metadata.functions["hello"] = {"url":"file:///C:/Users/kunle/Documents/GitHub/semicas/netlify/edge-functions/hello.js"}
         } else {
           console.log("\u001b[91m⬥\u001b[39m \u001b[31mFailed\u001b[39m to load Edge Function \u001b[33mhello\u001b[39m. The file does not seem to have a function as the default export.");
         }
       } catch (error) {
         console.log("\u001b[91m⬥\u001b[39m \u001b[31mFailed\u001b[39m to run Edge Function \u001b[33mhello\u001b[39m:");
+        console.error(error);
+      }
+      
+
+
+      try {
+        const { default: func } = await import("file:///C:/Users/kunle/Documents/GitHub/semicas/netlify/edge-functions/webhook.js");
+
+        if (typeof func === "function") {
+          functions["webhook"] = func;
+          metadata.functions["webhook"] = {"url":"file:///C:/Users/kunle/Documents/GitHub/semicas/netlify/edge-functions/webhook.js"}
+        } else {
+          console.log("\u001b[91m⬥\u001b[39m \u001b[31mFailed\u001b[39m to load Edge Function \u001b[33mwebhook\u001b[39m. The file does not seem to have a function as the default export.");
+        }
+      } catch (error) {
+        console.log("\u001b[91m⬥\u001b[39m \u001b[31mFailed\u001b[39m to run Edge Function \u001b[33mwebhook\u001b[39m:");
         console.error(error);
       }
       
